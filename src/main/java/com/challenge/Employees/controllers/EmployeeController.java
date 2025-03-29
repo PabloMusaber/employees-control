5e5fa658096021bb8dee656d954c4efd10a6ae0c
PabloMusaber <musaberpablo@gmail.com>
2025-03-28 21:36:22 -0300

    feat(docs): add summary for each endpoint

--- src/main/java/com/challenge/Employees/controllers/EmployeeController.java
+++ src/main/java/com/challenge/Employees/controllers/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.challenge.Employees.controllers;
 
 import com.challenge.Employees.services.employee.EmployeeService;
+
+import io.swagger.v3.oas.annotations.Operation;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,31 +21,37 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
+    @Operation(summary = "Obtiene los segmentos de salario A, B y C")
     @GetMapping("/salary-segments")
     public ResponseEntity<List<SalarySegmentDTO>> getSalarySegmentDistribution() {
         return ResponseEntity.ok(employeeService.getSalarySegmentDistribution());
     }
 
+    @Operation(summary = "Obtiene los segmentos de salario A, B y C por departamento")
     @GetMapping("/salary-segments-by-department")
     public ResponseEntity<List<DepartmentSalarySegmentDTO>> getSalarySegmentByDepartment() {
         return ResponseEntity.ok(employeeService.getSalarySegmentByDepartment());
     }
 
+    @Operation(summary = "Obtiene los empleados mejores pagos de cada departamento")
     @GetMapping("/highest-paid-per-department")
     public ResponseEntity<List<HighestPaidEmployeeDTO>> getHighestPaidEmployeePerDepartment() {
         return ResponseEntity.ok(employeeService.getHighestPaidEmployeePerDepartment());
     }
 
+    @Operation(summary = "Obtiene los managers con más de 15 años de antigüedad.")
     @GetMapping("/veteran-managers")
     public ResponseEntity<List<VeteranManagerDTO>> getVeteranManagers() {
         return ResponseEntity.ok(employeeService.getVeteranManagers());
     }
 
+    @Operation(summary = "Obtiene el salario promedio de todos los departamentos con más de 10 empleados")
     @GetMapping("/departments-with-10-plus-employees")
     public ResponseEntity<List<DepartmentAverageSalaryDTO>> getDepartmentsWithMoreThan10Employees() {
         return ResponseEntity.ok(employeeService.getDepartmentsWithMoreThan10Employees());
     }
 
+    @Operation(summary = "Obtiene información de empleados agrupada por país")
     @GetMapping("/country-employee-stats")
     public ResponseEntity<List<CountryEmployeeStatsDTO>> getCountryEmployeeStatistics() {
         return ResponseEntity.ok(employeeService.getCountryEmployeeStatistics());
